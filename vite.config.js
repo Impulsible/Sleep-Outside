5752d4fd88302482d19bf00c253ebff02b3ec682
Henry Osuagwu <henryosuagwu@yahoomail.com>
2025-09-20 00:10:53 +0100

    chore: update vite.config.js for product_listing page

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  root: 'src',
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        product_listing: resolve(__dirname, 'src/product_listing/index.html'),
+        main: 'src/index.html',
+        product_listing: 'src/product_listing/index.html',
       }
     }
   }
 });
-
-
